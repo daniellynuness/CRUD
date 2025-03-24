8fdb6552b18a6988b360280b34da2df819d67f46
dani <danym.nunes@gmail.com>
2025-03-24 18:54:19 -0500

    finalização crud

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
-//aplicação create
+const { where } = require('sequelize');
+
+//aplicação
 (async () => {
     const datebase = require('./db');
     const Produto = require('./produto');
@@ -7,14 +9,28 @@
         const resultado = await datebase.sync();
         console.log(resultado);
 
-        const resultadoCreate = await Produto.create({
+    //create
+        /*const resultadoCreate = await Produto.create({
             nome: 'mouse',
             preco: 10,
             descricao: 'Um mouse USB bonitão'
         })
-        console.log(resultadoCreate);
+        console.log(resultadoCreate);*/
     } 
     catch (error) {
         console.log(error);
     }
+
+    //read
+    const produtos = await Produto.findAll();
+    console.log(produtos);
+
+    //update
+    /*const produto = await Produto.findByPk(1);
+    produto.nome = "Mouse Top";
+    const resultadoSave = await produto.save();
+    console.log(resultadoSave);*/
+
+    //delete
+    Produto.destroy({where: {id:2}});
 })();
